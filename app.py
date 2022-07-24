79bc3f1d8162bb51524e2811ac4aecb7f27abaa0
aadhavic <96883763+aadhavic@users.noreply.github.com>
2022-07-24 23:48:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+def calculate(a,b):
+  try:
+    ans=a/b
+    st.write("""Ans is """+ ans)
+  except:
+    st.write("Denomitor can't be 0")
+st.write("""
+# Division of 2 numbers
+# """)
+a=st.number_input("Numerator")
+b=st.number_input("Denominator")
+st.button("Calculate", on_click=calculate(a,b))
